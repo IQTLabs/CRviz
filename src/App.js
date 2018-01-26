c62270a85fd4f2b5717aca67f0f84301adf2440a
Mark Nguyen <pnguyen1097@gmail.com>
2018-01-26 14:05:24 -0500

    Set up redux and a basic dataset reducer.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import React, { Component } from "react";
 
+import { Provider } from 'react-redux';
+
+import configureStore from './configure-store';
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.store = configureStore();
+  }
+
+  componentDidMount() {
+    this.store.dispatch({
+      type: 'SET_DATASET',
+      payload: {
+        dataset: [
+          {
+            uid: 'uid',
+            role: {
+              role: 'workstation',
+              confidence: 91
+            }
+          },
+          {
+            uid: 'uid2',
+            role: {
+              role: 'workstation',
+              confidence: 91
+            }
+          }
+        ],
+        configuration: {}
+      }
+    })
+  }
+
   render() {
-    return <div className="App">App</div>;
+    return (
+      <Provider store={ this.store }>
+        <div className="App">App</div>
+      </Provider>
+    );
   }
 }
 
---
+++ src/domain/root-reducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import dataset from './dataset';
+
+export default combineReducers({
+  dataset
+});
