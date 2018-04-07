d0b003fc2de1e6d6f403cf3212b2a9482a6c76f4
Ryan Ashley <rashley@iqt.org>
2018-04-06 21:23:06 -0400

    Fixes #33 - removed uneccessary validation and correctly reformatted data for case of single object

--- src/epics/load-dataset-epic.js
+++ src/epics/load-dataset-epic.js
@@ -1,7 +1,7 @@
 import { createAction } from "redux-actions";
 import { Observable } from "rxjs";
 
-import { isNil, is, isEmpty } from "ramda";
+import { isNil, is } from "ramda";
 
 import { setDataset } from "domain/dataset";
 import { setHierarchyConfig, colorBy } from "domain/controls";
@@ -13,7 +13,6 @@ const loadDatasetEpic = (action$, store) => {
     .ofType(loadDataset.toString())
     .mergeMap(({ payload }) => {
       return Observable.of(payload)
-        .do(validate)
         .do(formatPayload)
         .map((payload) =>
           setDataset({
@@ -33,34 +32,19 @@ const loadDatasetEpic = (action$, store) => {
     });
 };
 
-const validate = (data) => {
-  const errors = [];
-
-  //in the case of a naked array the array will be implicitly placed into a suboject named dataset
-  //hence the check on data.data
-  console.log(data);
-  if ((isNil(data.dataset) || !is(Array, data.dataset)) && !is(Array, data.data)) {
-    errors.push(
-      "Data must contain a key named dataset whose value is an array or be an array itself."
-    );
-  }
-
-  if (!isEmpty(errors)) {
-    throw new ValidationError(errors.join(" "));
-  }
-};
-
-//if we have a naked array instead of an object containing a dataset
+//if we have a naked array or an object not containing a dataset instead of an object containing a dataset
 //transfer the array into an object's dataset to maintain a consistent
 //schema with what is used elsewhere see https://github.com/CyberReboot/CRviz/issues/33
 const formatPayload = (data) => {
-  var tempDataset = [];
-  if(isNil(data.dataset) && is(Array, data.data)){
-    tempDataset = data.data;
+  var temp = {};
+  if(isNil(data.dataset) && is(Array, data.dataset)){
+    temp.dataset = data.dataset;
+  } else if(isNil(data.dataset) && is(Array, data)) {
+    temp.dataset = data;
   } else {
-    tempDataset = data.dataset;
+    temp.dataset = Object.keys(data).map( (key) =>{ return [key, data[key]]  })
   }
-  data.dataset = tempDataset;
+  data.dataset = temp.dataset;
 };
 
 function ValidationError(message) {
