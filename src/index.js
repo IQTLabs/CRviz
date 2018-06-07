b4d4820a5ab01d5c7f2659fff4e1f7af86ce14e7
Ryan Ashley <rashley@iqt.org>
2018-06-07 14:21:25 -0400

    -fixed css styling issues

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
-import "./index.css";
+import "index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
