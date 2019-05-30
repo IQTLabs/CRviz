dabb9e5b96a81a0245c7675cc5939a390bc8d915
rashley-iqt <rashley@iqt.org>
2019-05-30 18:22:51 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CRviz/precache-manifest.2b9384112f7df280d5ed48e685a10d8f.js"
+  "/CRviz/precache-manifest.ad41d3a06c91414227d3abac0adc87b2.js"
 );
 
 self.addEventListener('message', (event) => {
