93df0dbb96e46361afec13e4afb2cb5b833ef49b
Hope Tambala <hopetambala@outlook.com>
2019-07-01 15:31:28 -0400

    chore(): add actions to notes.js

--- src/domain/notes.js
+++ src/domain/notes.js
@@ -3,12 +3,13 @@ import { createAction, handleActions } from "redux-actions";
 const defaultState = {
   byId:["1"],
   byHash: {
-    id: "1", 
-    nodeID:null,
-    note: {
-      title: 'item 1',
-      labels: ['weird','red','interesting'],
-      content:"I'm a note!"
+    "1":  {
+      id:"1",
+      note: { 
+        title: 'item 1',
+        labels: ['weird','red','interesting'],
+        content:"I'm a note!"
+      }
     }
   },
 };
@@ -20,7 +21,7 @@ const removeAllNotes = createAction("REMOVE_ALL_NOTES");
 
 const addLabel = createAction("ADD_LABEL");
 const removeLabel = createAction("REMOVE_LABEL");
-const removeAllLabels = createAction("REMOVE_ALL_LABELS");
+//const removeAllLabels = createAction("REMOVE_ALL_LABELS"); //rope this into removeAllNotes
 
 
 // REDUCERS
@@ -34,11 +35,17 @@ const reducer = handleActions(
           [payload.id]: payload
         }
       }
-
     },
-    [removeNote]: (state) => {
-     	const noteString = "";
-      return { ...state, noteString };
+    [removeNote]: (state, { payload }) => {
+      const prunedIds = state.byId.filter(item => {
+        return item !== payload.id // return all the items not matching the action.id
+      })
+      delete state.byHash[payload.id] // delete the hash associated with the action.id
+      
+      return {
+        byId: prunedIds,
+        byHash: state.byHash
+      }
     }
   },
   defaultState
@@ -50,4 +57,9 @@ const getLabels = (state) => state.notes.notes;
 
 export default reducer;
 
-export { addNote, removeNote, getNotes};
+export { addNote, removeNote,removeAllNotes, getNotes};
+
+/*
+* REFERENCE
+* https://hackernoon.com/redux-patterns-add-edit-remove-objects-in-an-array-6ee70cab2456
+*/
