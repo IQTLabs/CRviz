cd0df85108f27cc0b3b857d9531e356b6238fb38
Hope Tambala <hopetambala@outlook.com>
2019-07-16 12:13:01 -0400

    chore(test): create a notes testing file to test notes reducer

---
+++ src/domain/notes.test.js.js
@@ -0,0 +1,95 @@
+import {
+  default as notes,
+  addNote,
+  setNotes,
+  removeNote, 
+  getAllNotes, 
+  getNotesIndexedByHash
+} from "./notes";
+import { combineReducers } from "redux";
+import { expect, assert } from "chai"
+
+const reducer = combineReducers({ notes });
+
+describe("Notes reducer", () => {
+  
+  describe("add and remove notes", () => {
+    it("add a note", (done) => {
+      const note = {
+        id :'TestId',
+        note:{
+          title: "I'm the Title of a Notes",
+          labels: ['label a', 'label b', 'label c'],
+          content:" I'm the Content of a note"
+        }
+      };
+
+      const action = addNote(note);
+      const result = reducer({}, action);
+
+      const defaultState = {
+        byId:[`TestId`],
+        byHash: {
+          'TestId': {
+            id: "TestId",
+            note:{
+              title: "I'm the Title of a Notes",
+              labels: ['label a', 'label b', 'label c'],
+              content:" I'm the Content of a note"
+            } 
+          }
+        }
+      };
+
+      expect(getAllNotes(result)).to.deep.equal(defaultState);
+
+      done();
+    });
+
+    it("remove a note", (done) => {
+      const note = {
+        id :'TestId',
+        note:{
+          title: "I'm the Title of a Notes",
+          labels: ['label a', 'label b', 'label c'],
+          content:" I'm the Content of a note"
+        }
+      };
+
+      const emptyState = {
+        byId:[],
+        byHash: {
+        }
+      };
+
+      const add_action = addNote(note);
+
+      const remove_action = removeNote(note.id);
+
+      var result = reducer({}, add_action);
+      var result = reducer({}, remove_action);
+
+      expect(getAllNotes(result)).to.deep.equal(emptyState);
+
+      done();
+    });
+  });
+
+  describe("setNotes", () => {
+    it("sets the Control tree", (done) => {
+      const notes = {
+        'byId':[],
+        'byHash': {
+        }
+      }
+
+      const action = setNotes(notes);
+      const result = reducer({}, action);
+
+      expect(getAllNotes(result)).to.deep.equal(notes);
+
+      done();
+    });
+  });
+
+});
