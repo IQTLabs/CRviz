e30b993b536213a6b8bce2b2025456b5d1c90c97
Hope Tambala <hopetambala@outlook.com>
2019-08-09 14:14:31 -0400

    fix():  make p a child of div (not the other way around)

--- src/features/tooltip/Tooltip.js
+++ src/features/tooltip/Tooltip.js
@@ -163,27 +163,24 @@ class TooltipControls extends React.Component {
     return (
       <>{this.props.data &&
         <div style={ this.state.show ? style.show : style.hide }>
-          <p>
-            {!this.state.show && <div className={tooltipStyle.hidden}><FontAwesomeIcon onClick={this.handleShowHide} icon={faAngleDoubleLeft} /> </div>}{this.state.show && <div className={tooltipStyle.shown}><FontAwesomeIcon onClick={this.handleShowHide} icon={faAngleDoubleRight} /></div>}
-          </p>
-          {this.props.data && !this.props.data.fieldValue  &&
-            <>
-              
-              <div>
-                <p><b>UID: </b>{this.props.data.uid} </p>
-                <p><b>MAC: </b>{this.props.data.mac} </p>
-                <p><b>rDNS_host: </b>{this.props.data.rDNS_host} </p>
-                <p><b>Subnet: </b>{this.props.data.subnet} </p>
-                <p><b>IP: </b>{this.props.data.IP} </p>
-                <p><b>Record Source: </b>{this.props.data.record.source} </p>
-                <p><b>Record Timestamp: </b>{this.props.data.record.timestamp} </p>
-                <p><b>Role: </b>{this.props.data.role.role} </p>
-                <p><b>rDNS_domain: </b>{this.props.data.rDNS_domain} </p>
-                <p><b>OS: </b>{this.props.data.os.os} </p>
-                <p><b>OS Confidence: </b>{this.props.data.os.confidence} </p>
-                <p><b>Vendor: </b>{this.props.data.vendor} </p>
-              </div>
-            </>
+          <div>
+            {!this.state.show && <p className={tooltipStyle.hidden}><FontAwesomeIcon onClick={this.handleShowHide} icon={faAngleDoubleLeft} /> </p>}{this.state.show && <p className={tooltipStyle.shown}><FontAwesomeIcon onClick={this.handleShowHide} icon={faAngleDoubleRight} /></p>}
+          </div>
+          {this.props.data && !this.props.data.fieldValue  &&              
+            <div>
+              <p><b>UID: </b>{this.props.data.uid} </p>
+              <p><b>MAC: </b>{this.props.data.mac} </p>
+              <p><b>rDNS_host: </b>{this.props.data.rDNS_host} </p>
+              <p><b>Subnet: </b>{this.props.data.subnet} </p>
+              <p><b>IP: </b>{this.props.data.IP} </p>
+              <p><b>Record Source: </b>{this.props.data.record.source} </p>
+              <p><b>Record Timestamp: </b>{this.props.data.record.timestamp} </p>
+              <p><b>Role: </b>{this.props.data.role.role} </p>
+              <p><b>rDNS_domain: </b>{this.props.data.rDNS_domain} </p>
+              <p><b>OS: </b>{this.props.data.os.os} </p>
+              <p><b>OS Confidence: </b>{this.props.data.os.confidence} </p>
+              <p><b>Vendor: </b>{this.props.data.vendor} </p>
+            </div>
           }
           {this.props.data && this.props.data.fieldValue &&
             <div>
