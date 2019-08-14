d3afc92c643bee2d21c329259328680ab6515015
Hope Tambala <hopetambala@outlook.com>
2019-08-14 09:44:11 -0400

    fix(): merge conflict

--- src/features/tooltip/Tooltip.js
+++ src/features/tooltip/Tooltip.js
@@ -125,26 +125,28 @@ class TooltipControls extends React.Component {
     const showNote = this.state.showNote;
 
 
-    return (<>{this.props.data &&
-        <div style={ this.state.show ? style.show : style.hide }>
+    return (
+      <>
+      {this.props.data &&
+        <div className={ this.state.show ? tooltipStyle.show : tooltipStyle.hide }>
           <div>
-            {!this.state.show && <p className={tooltipStyle.hidden}><FontAwesomeIcon onClick={this.handleShowHide} icon={faAngleDoubleLeft} /> </p>}{this.state.show && <p className={tooltipStyle.shown}><FontAwesomeIcon onClick={this.handleShowHide} icon={faAngleDoubleRight} /></p>}
+            {!this.state.show && <div className={tooltipStyle.hidden}><FontAwesomeIcon onClick={this.handleShowHide} icon={faAngleDoubleLeft} /> </div>}{this.state.show && <div className={tooltipStyle.shown}><FontAwesomeIcon onClick={this.handleShowHide} icon={faAngleDoubleRight} /></div>}
           </div>
-          {this.props.data && !this.props.data.fieldValue  &&              
-            <div>
-              <p><b>UID: </b>{this.props.data.uid} </p>
-              <p><b>MAC: </b>{this.props.data.mac} </p>
-              <p><b>rDNS_host: </b>{this.props.data.rDNS_host} </p>
-              <p><b>Subnet: </b>{this.props.data.subnet} </p>
-              <p><b>IP: </b>{this.props.data.IP} </p>
-              <p><b>Record Source: </b>{this.props.data.record.source} </p>
-              <p><b>Record Timestamp: </b>{this.props.data.record.timestamp} </p>
-              <p><b>Role: </b>{this.props.data.role.role} </p>
-              <p><b>rDNS_domain: </b>{this.props.data.rDNS_domain} </p>
-              <p><b>OS: </b>{this.props.data.os.os} </p>
-              <p><b>OS Confidence: </b>{this.props.data.os.confidence} </p>
-              <p><b>Vendor: </b>{this.props.data.vendor} </p>
-            </div>
+          {this.props.data && !this.props.data.fieldValue  &&
+              <div>
+                <p><b>UID: </b>{this.props.data.uid} </p>
+                <p><b>MAC: </b>{this.props.data.mac} </p>
+                <p><b>rDNS_host: </b>{this.props.data.rDNS_host} </p>
+                <p><b>Subnet: </b>{this.props.data.subnet} </p>
+                <p><b>IP: </b>{this.props.data.IP} </p>
+                <p><b>Record Source: </b>{this.props.data.record.source} </p>
+                <p><b>Record Timestamp: </b>{this.props.data.record.timestamp} </p>
+                <p><b>Role: </b>{this.props.data.role.role} </p>
+                <p><b>rDNS_domain: </b>{this.props.data.rDNS_domain} </p>
+                <p><b>OS: </b>{this.props.data.os.os} </p>
+                <p><b>OS Confidence: </b>{this.props.data.os.confidence} </p>
+                <p><b>Vendor: </b>{this.props.data.vendor} </p>
+              </div>
           }
           {this.props.data && this.props.data.fieldValue &&
             <div>
@@ -153,14 +155,14 @@ class TooltipControls extends React.Component {
           }
           {!this.props.data.fieldValue  &&
             <p className={appStyle.accordionHeader} onClick={this.toggleShowNote}>
-              Note {!showNote && <FontAwesomeIcon icon={faAngleDoubleDown} />}{showNote && <FontAwesomeIcon  onClick={this.toggleShowNote} icon={faAngleDoubleUp} />}
+              Notes {!showNote && <FontAwesomeIcon icon={faAngleDoubleDown} />}{showNote && <FontAwesomeIcon  onClick={this.toggleShowNote} icon={faAngleDoubleUp} />}
             </p>
           }
           
           {showNote === true &&
           <div>
-            <b><h1><input style={inputStyle} type="text" value={this.state.note.note.title} onChange={this.handleChangeTitle} placeholder="Title"/></h1></b>
-            <p><textarea style={inputStyle} type="text" value={this.state.note.note.content} onChange={this.handleChangeContent} placeholder="Take a note..."/></p>
+            <b><h1><input className={tooltipStyle.inputStyle} type="text" value={this.state.note.note.title} onChange={this.handleChangeTitle} placeholder="Title"/></h1></b>
+            <p><textarea className={tooltipStyle.inputStyle} type="text" value={this.state.note.note.content} onChange={this.handleChangeContent} placeholder="Take a note..."/></p>
             <div style={{textAlign:"center"}}>
               <label className="button circular">
                 <FontAwesomeIcon style={{margin:"2.5px"}} icon={faSave} onClick={this.saveNote}/>
@@ -171,7 +173,8 @@ class TooltipControls extends React.Component {
             </div>
           </div>
           }
-        </div>}
+        </div>
+        }
       </>
     );
   }
